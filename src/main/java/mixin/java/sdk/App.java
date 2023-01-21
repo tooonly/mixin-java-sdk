e8296f89b5b2fbac14c62fd9511e30aa3d99bb23
tooonly <aliyun_killer@yahoo.com>
2023-01-21 15:48:02 +0800

    App重写说明

--- src/main/java/mixin/java/sdk/App.java
+++ src/main/java/mixin/java/sdk/App.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class App {
+    /**
+     * 这个类只是一个单例Demo，需要运行多机器人时，需要自己重写一个类，
+     * MixinWebsocketListener每次都获取一个新的bean，类似于从BeanUtils.getBean(Class<T> clazz)获取一个新的bean
+     */
 
     @Autowired
     private MixinWebsocketListener mixinWebsocketListener;
